6153a728f234eec1f32b3c5397ee83d58295e170
Aman3214 <aman48anand@gmail.com>
2026-05-16 23:38:21 +0530

    added a cmake functionality

---
+++ build/CMakeFiles/myshell.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gojo/Documents/fun/commands.c" "CMakeFiles/myshell.dir/commands.c.o" "gcc" "CMakeFiles/myshell.dir/commands.c.o.d"
+  "/home/gojo/Documents/fun/main.c" "CMakeFiles/myshell.dir/main.c.o" "gcc" "CMakeFiles/myshell.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/myshell.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myshell.dir/commands.c.o"
+  "CMakeFiles/myshell.dir/commands.c.o.d"
+  "CMakeFiles/myshell.dir/main.c.o"
+  "CMakeFiles/myshell.dir/main.c.o.d"
+  "myshell"
+  "myshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
